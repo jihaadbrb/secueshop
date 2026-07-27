4acc99c1d92c38bebaae3b65a270c8f287a4b7e9
Jihad Bourbab <jihadbourbab79@gmail.com>
2026-07-27 08:03:59 -0400

    vulnerable version:intentional security flaws for pipeline demo

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,6 +15,8 @@ class AdminController extends Controller
     }
     $users = DB::select("SELECT id, name, email, created_at FROM users");
     return view('admin.users', compact('users'));
-    $api_key="ghp_abc123supersecrectkey987654ABCDEF";
+    $api_key = "ghp_abc123supersecrectkey987654ABCDEF";
+    $aws_key = "AKIAIOSFODNN7EXAMPLE1234";
+    $aws_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
 }
 }
