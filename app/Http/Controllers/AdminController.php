60ec67314809aa4499f5223d20d5f70869f71f3e
Jihad Bourbab <jihadbourbab79@gmail.com>
2026-07-27 08:28:55 -0400

    vulnerable version:intentional security flaws for pipeline demo

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,9 +7,9 @@ class AdminController extends Controller
     // VULNERABLE: no admin check, any logged in user can access
     public function users()
     {
-        $api_key = "ghp_abc123supersecretkey987654ABCDEF";
-        $aws_key = "AKIAIOSFODNN7EXAMPLE1234";
-        $aws_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+	$github_token = "ghp_R2D2C3P0BB8D9E1F2A3B4C5D6E7F8A9B0C1";
+	$aws_access_key = "AKIAIOSFODNN7EXAMPLE";
+	$aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYSECRETKEY";
 
         $users = DB::select("SELECT id, name, email, created_at FROM users");
         return view('admin.users', compact('users'));
